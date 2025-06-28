0e7a0a5b3d31a6886ccf0f36f613c1b191d89d24
PEEWEE\petra <petravos70@gmail.com>
2025-06-28 20:40:21 +0200

    Update config.

--- next-intl.config.ts
+++
@@ -1,5 +0,0 @@
-import NextIntlConfig from 'next-intl/plugin';
-
-const config = NextIntlConfig({});
-
-export default config;
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
@@ -13,6 +15,5 @@ const nextConfig: NextConfig = {
     ],
   },
 };
-const withNextIntl = createNextIntlPlugin();
 
 export default withNextIntl(nextConfig);
