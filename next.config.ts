05497dc0a05bd3d1fdec79590d45ff601c27bb89
PEEWEE\petra <petravos70@gmail.com>
2025-06-30 11:33:22 +0200

    Update config file.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/pokemon-fan-page',
   images: {
     unoptimized: true,
     remotePatterns: [
