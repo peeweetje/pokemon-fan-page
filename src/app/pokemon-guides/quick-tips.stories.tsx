27ceab4ec1061748368dcea3aeb577cc5842c048
PEEWEE\petra <petravos70@gmail.com>
2026-01-13 11:30:28 +0100

    Add story for the quick tips. Pokemon guides.

---
+++ src/app/pokemon-guides/quick-tips.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import QuickTips from './quick-tips';
+
+const meta = {
+    title: 'Pokemon Guides/QuickTips',
+    component: QuickTips,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    decorators: [
+        (Story) => (
+            <div className="w-[400px]">
+                <Story />
+            </div>
+        ),
+    ],
+} satisfies Meta<typeof QuickTips>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
