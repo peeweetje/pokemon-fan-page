1c97c8f78554eb37d7d44c2b5275ff97b81d3154
PEEWEE\petra <petravos70@gmail.com>
2026-01-14 12:02:49 +0100

    Add story for the quiz options.

---
+++ src/app/pokemon-quiz/pokemon-quiz-options.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { PokemonQuizOptions } from './pokemon-quiz-options';
+
+
+const meta = {
+    title: 'Pokemon Quiz/PokemonQuizOptions',
+    component: PokemonQuizOptions,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        selectedAnswer: { control: 'text' },
+        showExplanation: { control: 'boolean' },
+    },
+    args: {
+        options: ['Bulbasaur', 'Charmander', 'Squirtle', 'Pikachu'],
+        correctAnswer: 'Pikachu',
+        onSelectAnswer: () => { },
+    },
+} satisfies Meta<typeof PokemonQuizOptions>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Unanswered: Story = {
+    args: {
+        selectedAnswer: null,
+        showExplanation: false,
+    },
+};
+
+export const CorrectAnswerSelected: Story = {
+    args: {
+        selectedAnswer: 'Pikachu',
+        showExplanation: true,
+    },
+};
+
+export const IncorrectAnswerSelected: Story = {
+    args: {
+        selectedAnswer: 'Charmander',
+        showExplanation: true,
+    },
+};
