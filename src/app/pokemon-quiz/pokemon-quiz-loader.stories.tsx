a753b4e9a2e0b20315d1ab284a5c5bdbe2c10301
PEEWEE\petra <petravos70@gmail.com>
2026-01-14 12:05:00 +0100

    Add story for the quiz loader.

---
+++ src/app/pokemon-quiz/pokemon-quiz-loader.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { PokemonQuizLoader } from './pokemon-quiz-loader';
+
+const meta = {
+    title: 'Pokemon Quiz/PokemonQuizLoader',
+    component: PokemonQuizLoader,
+    parameters: {
+        layout: 'fullscreen',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof PokemonQuizLoader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
